e9f515e1159e4ce44874d7ff48973f841f9e5a72
Saurabh <dubeysaurabhrajendra@gmail.com>
2025-06-02 16:18:16 +0530

    Deleted all files

--- lbs_app/lbs_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('library_app.urls')),  # Replace 'library_app' with your app's name
-]
